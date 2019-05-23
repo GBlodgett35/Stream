ceec0fe95d27419422dfa4db9c9ed0a8fb069204
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2019-05-22 21:30:44 -0400

    Update and rename IntStream.h to IntStream.cpp

--- IntStream.cpp
+++ IntStream.cpp
@@ -1,6 +1,23 @@
-#include "BaseStream.cpp" 
+/*
+Author: Greg Blodgett
 
-class IntStream : BaseStream<int> {
-	int* toArray();
+The int specialization for the BaseStream class. 
+*/
 
-};
+#include "IntStream.h"
+
+/*
+  A method to return an Array consisting of the elements of the Stream
+  Returns: An int* 
+*/
+const int* IntStream::toArray() const {
+	return &elements[0];
+}
+
+/*
+  A method that returns the count of the elements in the Stream
+  Returns: The size of the Stream
+*/
+int IntStream::count() const {
+	return elements.size();
+}
