92de0c7d78fa350249d7e946f73175e7efa80a26
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2019-05-23 12:58:36 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,16 +7,18 @@ An example usage of the classes
 #include<iostream>
 #include<string>
 #include "Stream.cpp"
+#include "IntStream.h"
 
 
 int main() {
 	auto lambda = [](std::string str) {return stoi(str); };
 	Stream<std::string> a{ std::vector<std::string>{"1", "2", "3"} };
-	
-	a.map(lambda).filter([](int e) {return e > 1; }).forEach(std::cout);
-	
-	
-	
+	int size = IntStream::range(0, 5).count();
+	std::string str = a.map([](std::string s) {return s + " "; }).findFirst("2 ").value_or("");
+	int x = Stream<std::string>::of("1", "2", "3", "4").mapToInt([](std::string str) {return stoi(str); }).max().value_or(-1);
+
+
+
 	std::cin.get();
 	return 0;
 }
