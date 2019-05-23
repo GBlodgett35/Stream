e9df45a2c5cb6b1e6664bf7a695f17f58f8818ed
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2019-05-22 21:31:50 -0400

    Update IntStream.h

--- IntStream.h
+++ IntStream.h
@@ -1,1 +1,16 @@
+/*
+Author: Greg Blodgett
+
+The int specialization of the BaseStream Class
+*/
+
 #pragma once
+#include "BaseStream.h" 
+#include<vector>
+
+class IntStream : BaseStream<int> {
+	const int* toArray() const;
+	int count() const;
+private:
+	const std::vector<int> elements;
+};
