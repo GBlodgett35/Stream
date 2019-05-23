695325950b1db8d5b5e8f4a181b8f50fbe02c7b9
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2019-05-23 18:58:00 -0400

    Update IntStream.h

--- IntStream.h
+++ IntStream.h
@@ -4,6 +4,7 @@ Author: Greg Blodgett
 The int specialization of the BaseStream Class
 */
 
+
 #pragma once
 #include "BaseStream.h" 
 #include<vector>
@@ -14,16 +15,19 @@ class IntStream : public BaseStream<int> {
 public:
 	IntStream(std::vector<int> const&);
 	IntStream operator = (IntStream const&);
+	IntStream(const IntStream&);
 	const int* toArray() const;
 	int count() const;
 	static IntStream range(int, int);
 	IntStream filter(std::function<bool(const int&)>) const;
-	bool noneMatch(int) const;
-	bool anyMatch(int) const;
-	bool allMatch(int) const;
+	bool noneMatch(std::function<bool(const int&)>) const;
+	bool anyMatch(std::function<bool(const int&)>) const;
+	bool allMatch(std::function<bool(const int&)>) const;
 	std::optional<int> max() const;
 	std::optional<int> min() const;
-	std::optional<int> findFirst(int) const;
+	std::optional<int> findFirst(std::function<bool(const int&)>) const;
+	void forEach(std::function<void(const int&)>) const;
+	static IntStream of(int args...);
 private:
 	const std::vector<int> elements;
 };
