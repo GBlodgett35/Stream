2a9026c952d1c524cd31c424be41dfbdc2dd0f2d
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2019-05-23 18:57:34 -0400

    Update IntStream.cpp

--- IntStream.cpp
+++ IntStream.cpp
@@ -1,12 +1,14 @@
 /*
 Author: Greg Blodgett
-
-The int specialization for the BaseStream class. 
+The int specialization of the BaseStream Class
 */
+
 
 #include "IntStream.h"
 #include<numeric>
 #include<functional>
+
+IntStream::IntStream(const IntStream &other) : elements(other.elements){}
 
 IntStream::IntStream(std::vector<int> const& vec) : elements(vec) {}
 
@@ -44,32 +46,83 @@ IntStream IntStream::filter(std::function<bool(const int&)> predicate) const
 	return IntStream{ intStream };
 }
 
-bool IntStream::allMatch(int target) const {
+bool IntStream::allMatch(std::function<bool(const int&)> target) const {
 	std::vector<int> newStream;
 	for (auto i = elements.begin(); i != elements.end(); ++i) {
-		if (*i != target) {
+		if (!target(*i)) {
 			return false;
 		}
 	}
 	return true;
 }
 
-bool IntStream::noneMatch(int target) const {
+bool IntStream::noneMatch(std::function<bool(const int&)> target) const {
 	std::vector<int> newStream;
 	for (auto i = elements.begin(); i != elements.end(); ++i) {
-		if (*i == target) {
+		if (target(*i)) {
 			return false;
 		}
 	}
 	return true;
 }
 
-bool IntStream::anyMatch(int target) const {
+bool IntStream::anyMatch(std::function<bool(const int&)> target) const {
 	std::vector<int> newStream;
 	for (auto i = elements.begin(); i != elements.end(); ++i) {
-		if (*i == target) {
+		if (target(*i)) {
 			return true;
 		}
 	}
 	return false;
+}
+
+std::optional<int> IntStream::max() const
+{
+	if (!elements.empty()) {
+		int _max = elements[0];
+		for (auto i = elements.begin(); i != elements.end(); ++i) {
+			if (*i > _max) {
+				_max = *i;
+			}
+		}
+		return std::optional<int>{_max};
+	}
+	return std::nullopt;
+}
+
+std::optional<int> IntStream::min() const
+{
+	if (!elements.empty()) {
+		int _min = elements[0];
+		for (auto i = elements.begin(); i != elements.end(); ++i) {
+			if (*i > _min) {
+				_min = *i;
+			}
+		}
+		return std::optional<int>{_min};
+	}
+	return std::nullopt;
+}
+
+std::optional<int> IntStream::findFirst(std::function<bool(const int&)> predicate ) const
+{
+	for (auto i = elements.begin(); i != elements.end(); ++i) {
+		if (predicate(*i)) {
+			int copy = *i;
+			return { copy };
+		}
+	}
+	return std::nullopt;
+}
+
+void IntStream::forEach(std::function<void(const int&)> consumer) const
+{
+	for (auto i = elements.begin(); i != elements.end(); ++i) {
+		consumer(*i);
+	}
+}
+
+IntStream IntStream::of(int args...)
+{
+	return IntStream(std::vector<int>{args});
 }
